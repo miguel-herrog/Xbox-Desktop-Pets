527cce76c01f3e87840c5fb4a80b399b54675d30
migue <migue@MIGUEL-HERROG>
2026-05-15 16:08:58 +0200

    Add CharacterModel with properties and basic validation

---
+++ XboxDesktopPets/Models/CharacterModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XboxDesktopPets.Models
+{
+    public class CharacterModel
+    {
+        public string Name { get; set; }
+        public float MoveSpeed { get; set; }
+        public string ImagePath { get; set; }
+        public string SoundPath { get; set; }
+        public bool IsFalling { get; set; }
+        public Saga Saga { get; set; }
+    }
+}
