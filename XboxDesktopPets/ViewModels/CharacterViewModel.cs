61bf113bf3a1ebec002e8381b1dac98951460315
migue <migue@MIGUEL-HERROG>
2026-05-15 16:48:52 +0200

    Added CharacterViewModel

---
+++ XboxDesktopPets/ViewModels/CharacterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XboxDesktopPets.Models;
+
+namespace XboxDesktopPets.ViewModels
+{
+    public class CharacterViewModel : ViewModelBase
+    {
+        // 1. Lo convertimos en Propiedad para que la interfaz lo pueda leer
+        public CharacterModel Character { get; set; }
+
+        // 2. Constructor sin parámetros (se ejecuta al abrir la app)
+        public CharacterViewModel()
+        {
+            // 3. Creamos la instancia
+            Character = new CharacterModel
+            {
+                Name = "Master Chief",
+                MoveSpeed = 5.0f,
+                IsFalling = false,
+                ImagePath = "Assets/Sprites/MasterChief_Idle.png",
+                SoundPath = "Assets/Sounds/Halo_Shield.wav",
+                Saga = Saga.Halo
+            };
+        }
+    }
+}
---
+++ XboxDesktopPets/ViewModels/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace XboxDesktopPets.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        // El evento que avisa a la interfaz de que algo ha cambiado
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // El método que dispara el aviso
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
